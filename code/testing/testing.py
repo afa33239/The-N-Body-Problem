4787fa93fd05c2821a4da53022aaa6d1c7193329
Ioan-Adrian Fanea <ec24483@qmul.ac.uk>
2025-12-16 17:05:03 +0000

    Ran tests with planar objects, next is non-planar

--- code/testing/testing.py
+++ code/testing/testing.py
@@ -1,14 +1,24 @@
 from code.nbody.bodies import Body
 from code.nbody.engine import Simulation, SimulationConfig
-from code.nbody.integrators.leapfrog import LeapfrogIntegrator 
+from code.nbody.integrators.leapfrog import LeapfrogIntegrator
 from code.nbody.integrators.euler import EulerIntegrator
 import matplotlib.pyplot as plt
 import math
 
-sun = Body(1.0, 0, 0, 0, -0.001 * 2 * math.pi)
-planet = Body(0.001, 1, 0, 0, 2 * math.pi)
+sun = Body(
+    1.0,
+    0.0, 0.0, 0.0,
+    0.0, -0.001 * 2 * math.pi, 0.0
+)
+
+planet = Body(
+    0.001,
+    1.0, 0.0, 0.0,
+    0.0, 2 * math.pi, 0.0
+)
 
 cfg = SimulationConfig(dt=0.0002, timesteps=5000, softening=0.0)
+
 sim = Simulation([sun, planet], cfg, integrator=LeapfrogIntegrator())
 sim.run()
 
@@ -16,18 +26,16 @@ plt.figure()
 plt.plot(sim.energy_drift)
 plt.title("Relative Energy Drift")
 
-
 plt.figure()
 plt.plot(sim.angular_momentum_drift)
 plt.title("Angular Momentum Drift")
-
 
 plt.figure()
 plt.plot(sim.linear_momentum_drift)
 plt.title("|P(t)|")
 
-
 plt.figure()
 plt.plot(sim.com_drift)
 plt.title("Center-of-Mass Drift")
+
 plt.show()
